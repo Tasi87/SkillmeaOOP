529f6861691a614d29a1c0aab51215eabf54b47d
Tasi <tasi87@gmail.com>
2025-06-03 20:39:05 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        //Random ranNum = new Random();
+
+        RodinnyDom domNovakovych = new RodinnyDom();
+        domNovakovych.pocetPoschodi = 4;
+        domNovakovych.rozloha = 457f;
+        domNovakovych.material = "mramor";
+        domNovakovych.pocetOkien = 16;
+
+        RodinnyDom domKralickovych = new RodinnyDom();
+        domKralickovych.pocetPoschodi = 3;
+        domKralickovych.rozloha = 657f;
+        domKralickovych.material = "tehla";
+        domKralickovych.pocetOkien = 12;
+
+        /*
+        String nejakyText = "niečo";
+                    =
+        String nejakyText = new String("niečo");
+        */
+
+
     }
 }
---
+++ src/RodinnyDom.java
@@ -0,0 +1,12 @@
+public class RodinnyDom {
+
+    int pocetPoschodi;
+    String material;
+    float rozloha;
+    int pocetOkien;
+
+    public void vypocetCenyDomu(){
+        System.out.println(rozloha * 8000);
+    }
+
+}
