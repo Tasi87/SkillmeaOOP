0628bb1b40097ac8e34e8eb7e061f48b85f02cdf
Tasi <tasi87@gmail.com>
2025-06-05 19:28:45 +0200

    Commit

--- src/ObchodnyDom.java
+++ src/ObchodnyDom.java
@@ -1,4 +1,19 @@
 public class ObchodnyDom {
+    private float rozloha;
+    private int pocetPoschodi;
+    private int pocetPokladni;
+
+    ObchodnyDom (float rozloha, int pocetPoschodi, int pocetPokladni) {
+        this.rozloha = rozloha;
+        this.pocetPoschodi = pocetPoschodi;
+        this.pocetPokladni = pocetPokladni;
+    }
+
+    public float vypocetCenyObchodu(){
+        float cena = (rozloha * pocetPoschodi) + 5000 * pocetPokladni;
+        System.out.println("Cena obchodu: " + cena);
+        return cena;
+    }
 
 
 
